e6147665b607164c957dccac6957bd6b15c2fa44
526371075@qq.com <q2670187>
2018-06-08 10:02:04 +0800

    about me added

--- code/src/app/app.router.ts
+++ code/src/app/app.router.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent }  from './home/home.component';
+import { HomeComponent } from './home/home.component';
 import { MeComponent } from './me/me.component';
 export const RootRoutes: Routes = [
-    {path:'', component: HomeComponent},
-    {path:'me', component: MeComponent}
-]
+    { path: '', component: HomeComponent },
+    { path: 'me', component: MeComponent }
+];
