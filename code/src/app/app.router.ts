2d5ad5a9b6150fd5c9b20d36557b7dd9e9e02802
mien <huangxiaomeng_vip@126.com>
2020-03-17 13:15:54 +0800

    remove old

--- code/src/app/app.router.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { HomeComponent } from './home/home.component';
-import { MeComponent } from './me/me.component';
-export const RootRoutes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'me', component: MeComponent }
-];
